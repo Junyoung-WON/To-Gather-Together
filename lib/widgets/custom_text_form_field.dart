4b347a8e81c5c617a35dd2232f1d185c3b8f687d
Junyoung-WON <wjyo60@gmail.com>
2023-05-17 00:28:04 +0900

    [STYLE] remove Material widget in custom textformfield file

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -21,58 +21,56 @@ class CustomTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: Text(
-                  label,
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w700
-                  ),
+    return Column(
+      children: [
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 8),
+              child: Text(
+                label,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700
                 ),
               ),
-            ],
-          ),
-          SizedBox(
-            height: 50,
-            child: TextFormField(
-              textAlignVertical: TextAlignVertical(y: 0.8),
-              onSaved: onSaved,
-              validator: validator,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                  borderSide: BorderSide(
-                    color: Colors.black45,
-                    width: 1.5,
-                    strokeAlign: BorderSide.strokeAlignCenter
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                  borderSide: BorderSide(
-                    color: Colors.amberAccent,
-                    width: 2,
-                    strokeAlign: BorderSide.strokeAlignCenter
-                  ),
-                ),
-                hintText: hintText,
-                hintStyle: TextStyle(
-                  color: Colors.black45,
-                ),
-              ),
-              textInputAction: textInputAction,
-              focusNode: focusNode,
-              onFieldSubmitted: onFieldSubmitted,
             ),
-          )
-        ],
-      ),
+          ],
+        ),
+        SizedBox(
+          height: 50,
+          child: TextFormField(
+            textAlignVertical: TextAlignVertical(y: 0.8),
+            onSaved: onSaved,
+            validator: validator,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                borderSide: BorderSide(
+                  color: Colors.black45,
+                  width: 1.5,
+                  strokeAlign: BorderSide.strokeAlignCenter
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                borderSide: BorderSide(
+                  color: Colors.amberAccent,
+                  width: 2,
+                  strokeAlign: BorderSide.strokeAlignCenter
+                ),
+              ),
+              hintText: hintText,
+              hintStyle: TextStyle(
+                color: Colors.black45,
+              ),
+            ),
+            textInputAction: textInputAction,
+            focusNode: focusNode,
+            onFieldSubmitted: onFieldSubmitted,
+          ),
+        )
+      ],
     );
   }
 }
